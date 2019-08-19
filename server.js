a68bd4d5841c05801608631298d5fcd1d6a22e21
Kay Wood <kay.wood@findel-education.co.uk>
2019-08-19 15:27:32 +0100

    Setup dockerfile

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('this works');
+});
+
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`);
